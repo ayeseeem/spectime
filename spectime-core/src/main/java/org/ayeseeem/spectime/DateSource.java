7ac89f3f0c0f0f356e586ee37cf1ec7d646f0e06
ayeseeem <ayeseeem@gmail.com>
2015-05-23 10:30:47 +0100

    Javadoc typo

--- spectime-core/src/main/java/org/ayeseeem/spectime/DateSource.java
+++ spectime-core/src/main/java/org/ayeseeem/spectime/DateSource.java
@@ -3,7 +3,7 @@ package org.ayeseeem.spectime;
 import java.util.Date;
 
 /**
- * Interface for isolating your code from time specific code - for example
+ * Interface for isolating your code from time-specific code - for example
  * <code>new Date()</code> - to help testing.
  */
 public interface DateSource {
