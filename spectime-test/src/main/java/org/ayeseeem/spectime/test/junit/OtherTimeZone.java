3d75ed4e731a0fb748a2d98558ea19e1555f8cf7
ayeseeem <ayeseeem@gmail.com>
2023-01-27 15:54:35 +0000

    refactor: move helper to OtherTimeZone
    The helper that gets a "definitely not default" time zone is moved to
    OtherTimeZone rule as it's much more related to - and used by - this
    rule. This rule relies on it, so it makes more sense to define it here.

--- spectime-test/src/main/java/org/ayeseeem/spectime/test/junit/OtherTimeZone.java
+++ spectime-test/src/main/java/org/ayeseeem/spectime/test/junit/OtherTimeZone.java
@@ -1,11 +1,25 @@
 package org.ayeseeem.spectime.test.junit;
 
+import static org.ayeseeem.spectime.test.ZonedTester.exclusiveZones;
+
 import java.util.TimeZone;
 
 public class OtherTimeZone extends RestoreTimeZone {
 
 	public OtherTimeZone() {
 		TimeZone.setDefault(definitelyNotDefaultTimeZone());
+	}
+
+	public static TimeZone definitelyNotDefaultTimeZone() {
+		TimeZone defaultZone = TimeZone.getDefault();
+
+		for (TimeZone zone : exclusiveZones()) {
+			if (!zone.equals(defaultZone)) {
+				return zone;
+			}
+		}
+
+		throw new RuntimeException("Failed to determine a time zone that was different from the default");
 	}
 
 }
